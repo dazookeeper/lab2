3917bababd3e930e4f8b233db02ee149a7868b2a
Kyle Zhu <kyzhu@ucsd.edu>
2016-01-14 08:40:36 +0000

    lab2 save 1

---
+++ heroku.sh
@@ -0,0 +1,7 @@
+Heroku create
+git push heroku master
+
+#Wait for the application to upload on heroku
+
+heroku confic:set YHAT_USERNAME=_yhat_username
+heroku config:set YHAT_APIKEY=your_yhat_api_key
